bd1b4c72f7d26727586e8df3960a3a55ec7fadf1
Mateusz Sygut <wrx76776@student.wroclaw.merito.pl>
2025-05-17 11:06:57 +0200

    Changed app title

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Ćwiczeniowa Aplikacja Reacta.
+          Zmieniony tytuł aplikacji
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Poznaj reakt
         </a>
       </header>
     </div>
