99074f1d62e2f8d0140795d5be8fd97a7ebbe45e
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-31 10:57:01 +0200

    Usuniete entery

--- calculator.py
+++ calculator.py
@@ -1,17 +1,12 @@
 class Calculator:
-
     def power(self, a, b):
         return a ** b
-
     def add(self, a, b):
         return a + b
-
     def subtract(self, a, b):
         return a - b
-
     def multiply(self, a, b):
         return a * b
-
     def divide(self, a, b):
         if b == 0:
             raise ValueError("Cannot divide by zero")
