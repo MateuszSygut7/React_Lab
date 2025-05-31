5be2a3295881fbcbb2819d6d919d9b522280d07c
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-05-31 10:46:57 +0200

    Dodany kalkulator

---
+++ test_calculator.py
@@ -0,0 +1,34 @@
+import unittest
+from calculator import Calculator
+
+class TestCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calc = Calculator()
+
+    def test_add(self):
+        self.assertEqual(self.calc.add(3, 4), 7)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 3), -3)
+
+    def test_multiply(self):
+        self.assertEqual(self.calc.multiply(3, 5), 15)
+        self.assertEqual(self.calc.multiply(-2, 4), -8)
+
+    def test_divide(self):
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertAlmostEqual(self.calc.divide(5, 2), 2.5)
+
+    def test_divide_by_zero(self):
+        with self.assertRaises(ValueError):
+            self.calc.divide(5, 0)
+    
+    def test_power(self):
+        self.assertEqual(self.calc.power(2, 3), 8)
+
+if __name__ == '__main__':
+    unittest.main()
+
+
